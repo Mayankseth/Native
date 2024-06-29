760d7d9aa615a14485ab850d60b4ce174ba25940
Mayankseth <sethmayank33@gmail.com>
2024-06-29 19:28:15 +0530

    api input

--- App.tsx
+++ App.tsx
@@ -30,35 +30,46 @@ import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs
 
 
 function App(): React.JSX.Element {
-  const [data,setData]= useState([])
-  
-  
-  const saveApiData = async() =>{
-    const data={
-      name:"Ap",
-      age:"22",
-      email:"ap@test"
-    }
+  const [name,setName] = useState()
+  const [age,setAge] = useState()
+  const [email,setEmail] = useState()
 
-    const url= "http://10.0.2.2:3000/users"
-    let result = await fetch(url ,{
+  const saveData = async ()=>{
+    const url="http://10.0.2.2:3000/users"
+    let result = await fetch(url, {
       method:"POST",
       headers:{"Content-Type":"application/json"},
-      body:JSON.stringify(data)
+      body: JSON.stringify({name,age,email})
     })
-    result=result.json();
-    console.warn(data)
-  } 
+
+    result=await result.json();
+    if(result){
+      console.warn("data entered")
+    }
+
+  }
 
   return (
    <View>
-    <Text style={{fontSize:20}}>Call json server api</Text>
-    <Button title='Save Data' onPress={saveApiData}/>
+    <Text style={{fontSize:20}}>Call json server with Input</Text>
+    <TextInput  placeholder='Enter Name' style={styles.input} value={name} onChangeText={(text)=> setName(text)}/>
+    <TextInput  placeholder='Enter Age' style={styles.input} value={age} onChangeText={(text)=> setAge(text)}/>
+    <TextInput  placeholder='Enter EMail' style={styles.input} value={email} onChangeText={(text)=> setEmail(text)}/>
+    <Button title='Save Data' onPress={saveData}/>
+
    
    </View>
    
   );
 };
+
+const styles = StyleSheet.create({
+  input:{
+    borderColor:"blue",
+    borderWidth:2,
+    margin:10
+  }
+})
 
 
 
