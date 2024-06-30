50e3888ed88bb06ef51ec10fe0041a9500173cfa
Mayankseth <sethmayank33@gmail.com>
2024-06-30 12:48:27 +0530

    api delete

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@
  */
 
 import React, { useEffect, useState } from 'react';
-import type {PropsWithChildren} from 'react';
+
 import {
   Button,
   FlatList,
@@ -19,90 +19,79 @@ import {
   StyleSheet,
   Text,
   TextInput,
-  TouchableHighlight,
-  TouchableOpacity,
-  useColorScheme,
+ 
   View,
 } from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
-import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs'
+
 
 
 function App(): React.JSX.Element {
-  const [name,setName] = useState()
-  const [age,setAge] = useState()
-  const [email,setEmail] = useState()
-
-  const [errorName , setNameError] = useState(false)
-  const [errorAge , setAgeError] = useState(false)
-  const [errorEmail , setEmailError] = useState(false)
+  const [data,setData] = useState([])
 
   const saveData = async ()=>{
-
-    if(!name){
-      setNameError(true)
-    }
-    else{
-      setNameError(false)
-    }
-    if(!age){
-      setAgeError(true)
-    }
-    else{
-      setAgeError(false)
-    }
-    if(!email){
-      setEmailError(true)
-    }
-    else{
-      setEmailError(false)
-    }
-    if(!name || !age || !email){
-      return false
-    }
+   
     const url="http://10.0.2.2:3000/users"
-    let result = await fetch(url, {
-      method:"POST",
-      headers:{"Content-Type":"application/json"},
-      body: JSON.stringify({name,age,email})
-    })
+    let result = await fetch(url)
 
-    result=await result.json();
+    result=await result.json()
     if(result){
-      console.warn("data entered")
+      setData(result)
     }
+  }
+  const deleteUser = async (id) =>{
+    const url="http://10.0.2.2:3000/users"
+    // console.warn(`${url}/${id}`)
+    let result = await fetch(`${url}/${id}`,{
+      method:"DELETE"
+    });
+    result= await result.json();
+    if(result){
+      console.warn('User Deleted')
+      saveData();
+    }
+    
 
   }
 
-  return (
-   <View>
-    <Text style={{fontSize:20}}>Call json server with Input</Text>
-    <TextInput  placeholder='Enter Name' style={styles.input} value={name} onChangeText={(text)=> setName(text)}/>
-      {
-        errorName? <Text style={{color:"red"}}> Enter valid Text</Text>:null
-      }
-    <TextInput  placeholder='Enter Age' style={styles.input} value={age} onChangeText={(text)=> setAge(text)}/>
-    {
-        errorAge? <Text style={{color:"red"}}> Enter valid Age</Text>:null
-      }
-    <TextInput  placeholder='Enter EMail' style={styles.input} value={email} onChangeText={(text)=> setEmail(text)}/>
-    {
-        errorEmail? <Text style={{color:"red"}}> Enter valid Email</Text>:null
-      }
-    <Button title='Save Data' onPress={saveData}/>
+  useEffect(()=>{
+    saveData();
+  },[])
 
-   
+  return (
+   <View style={styles.container}>
+    <View style={styles.dataWrapper}>
+          <View style={{flex:1}}><Text>Name</Text></View>
+          <View style={{flex:2}}><Text>Age</Text></View>
+          <View style={{flex:2}}><Text>Operations</Text></View>
+        
+        </View>
+
+    {
+      data.length?
+        data.map((item)=><View style={styles.dataWrapper}>
+          <View style={{flex:1}}><Text>{item.name}</Text></View>
+          <View style={{flex:1}}><Text>{item.age}</Text></View>
+          <View style={{flex:1}}><Button title='Update'/></View>
+          <View style={{flex:1}}><Button title='Delete' onPress={()=>deleteUser(item.id)}/></View>
+        </View>)
+
+        :null
+    }
    </View>
    
   );
 };
 
 const styles = StyleSheet.create({
-  input:{
-    borderColor:"blue",
-    borderWidth:2,
-    margin:10
+  container:{
+    flex:1
+  },
+  dataWrapper:{
+    flexDirection:"row",
+    justifyContent:"space-around",
+    backgroundColor:"orange",
+    margin:5,
+    padding:10
   }
 })
 
