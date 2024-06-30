9e74a1755c0141224c03a295b78e08ea09a79720
Mayankseth <sethmayank33@gmail.com>
2024-06-30 21:01:36 +0530

    api search

--- App.tsx
+++ App.tsx
@@ -26,153 +26,38 @@ import {
 
 
 function App(): React.JSX.Element {
-  const [data,setData] = useState([])
-  const [showModal,setShowModal] = useState(false)
-  const [selectedUser,setSelecteduser] = useState(undefined)
-
-  const saveData = async ()=>{
-   
-    const url="http://10.0.2.2:3000/users"
+  const [data,setData]=useState([])
+  
+  const searchUser = async(text) =>{
+    const url= `http://10.0.2.2:3000/users?q=${text}`;
     let result = await fetch(url)
-
-    result=await result.json()
+    result= await result.json();
     if(result){
       setData(result)
     }
   }
-  const deleteUser = async (id) =>{
-    const url="http://10.0.2.2:3000/users"
-    // console.warn(`${url}/${id}`)
-    let result = await fetch(`${url}/${id}`,{
-      method:"DELETE"
-    });
-    result= await result.json();
-    if(result){
-      console.warn('User Deleted')
-      saveData();
-    }
-    
-
-  }
-
-  useEffect(()=>{
-    saveData();
-  },[])
-
-  const updateUser = (data)=>{
-    setShowModal(true)
-    setSelecteduser(data)
-
-  }
 
   return (
-   <View style={styles.container}>
-    <View style={styles.dataWrapper}>
-          <View style={{flex:1}}><Text>Name</Text></View>
-          <View style={{flex:2}}><Text>Age</Text></View>
-          <View style={{flex:2}}><Text>Operations</Text></View>
-        
-        </View>
-
-    {
-      data.length?
-        data.map((item)=><View style={styles.dataWrapper}>
-          <View style={{flex:1}}><Text>{item.name}</Text></View>
-          <View style={{flex:1}}><Text>{item.age}</Text></View>
-          <View style={{flex:1}}><Button title='Update' onPress={()=>updateUser(item)}/></View>
-          <View style={{flex:1}}><Button title='Delete' onPress={()=>deleteUser(item.id)}/></View>
-        </View>)
-
-        :null
-    }
-    <Modal visible={showModal} transparent={true}>
-     <UserModal setShowModal={setShowModal} selectedUser={selectedUser} saveData={saveData}/>
-    </Modal>
-   </View>
+    <View>
+      <Text style={{fontSize:25}}>Search with Api</Text>
+      <TextInput style={{borderColor:"skyblue", borderWidth:1, margin:10, fontSize:20}} placeholder={"search"}
+        onChangeText={(text)=>searchUser(text)}/>
+        {
+          data.length?
+          data.map((item)=>
+              <View style={{padding:10, justifyContent:"space-around",flexDirection:"row"}}>
+                <Text>{item.name}</Text>
+                <Text>{item.age}</Text>
+                <Text>{item.email}</Text>
+              </View>
+        ):null
+        }
+    </View>
    
   );
 };
 
-const UserModal = (props) => {
-  const [name,setName] = useState()
-  const [age,setAge] = useState()
-  const [email,setEmail] = useState()
 
-  useEffect(()=>{
-    if(props.selectedUser){
-      setName(props.selectedUser.name)
-      setAge(props.selectedUser.age.toString())
-      setEmail(props.selectedUser.email)
-
-    }
-
-  },[props.selectedUser])
-
-  const updateUser = async()=>{
-    const url="http://10.0.2.2:3000/users";
-    const id = props.selectedUser.id
-    let result = await fetch(`${url}/${id}`,{
-      method:"PUT",
-      headers:{
-        "Content-Type":"application/json"
-      },
-      body:JSON.stringify({name,age,email})
-    })
-    result=await result.json()
-    if(result){
-    console.warn(result)
-    props.saveData();
-    props.setShowModal(false);
-
-  }
-}
-  return (
-    <View style={styles.centeredView}>
-        <View style={styles.modalView}>
-          <TextInput style={styles.input} value={name} onChangeText={(text)=>setName(text)}/>
-          <TextInput style={styles.input} value={age} onChangeText={(text)=>setAge(text)}/>
-          <TextInput style={styles.input} value={email} onChangeText={(text)=>setEmail(text)}/>
-          <View style={{marginBottom:10}}><Button onPress={updateUser} title='Save'/></View>
-          <Button title='Close' onPress={()=>props.setShowModal(false)}/>
-        </View>
-      </View>
-  )
-
-
-}
-
-const styles = StyleSheet.create({
-  container:{
-    flex:1
-  },
-  dataWrapper:{
-    flexDirection:"row",
-    justifyContent:"space-around",
-    backgroundColor:"orange",
-    margin:5,
-    padding:10
-  },
-  centeredView:{
-    flex:1,
-    alignItems:"center",
-    justifyContent:"center"
-  },
-  modalView:{
-    backgroundColor:"#fff",
-    borderRadius:10,
-    padding:60,
-    shadowColor:"#000",
-    shadowOpacity:0.7,
-    elevation:5
-  },
-  input:{
-    borderWidth:1,
-    borderColor:"skyblue",
-    width:300,
-    marginBottom:15
-
-  }
-})
 
 
 
